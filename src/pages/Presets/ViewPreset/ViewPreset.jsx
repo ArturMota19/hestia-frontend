8b5d1ca0b5634958bb3594e0b19e5ac1a2c3c80e
ArturMota19 <artur19rodriguesmota@gmail.com>
2025-05-14 14:02:37 -0300

    FEAT: Finish set and get to presets

--- src/pages/Presets/ViewPreset/ViewPreset.jsx
+++ src/pages/Presets/ViewPreset/ViewPreset.jsx
@@ -16,6 +16,8 @@ export default function ViewPreset() {
 	const { t } = useTranslation();
   const [currentPage, setCurrentPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false)
+  const [housePresets, setHousePresets] = useState([])
+  const [itemsCount, setItemsCount] = useState(1)
   const itemsPerPage = 6;
 
   async function FetchData(){
@@ -25,75 +27,37 @@ export default function ViewPreset() {
       isAuth: true,
       setIsLoading
     })
-    console.log(response)
+    if (response.status === 200) {
+      setItemsCount(response.data.count);
+      const updatedPresets = response.data.presetData.map((preset) => {
+      // Get each Room
+      const rooms = [];
+      preset.HouseRooms.forEach((room) => {
+        let stringActuators = "";
+        room.RoomActuators.forEach((actuator, index) => {
+        stringActuators += actuator.name;
+        if (index < room.RoomActuators.length - 1) {
+          stringActuators += ", ";
+        }
+        });
+        const finalString = `${room.Room.name} (${stringActuators})`;
+        rooms.push(finalString);
+      });
+      return {
+        paramName: preset.name,
+        rooms,
+        type: "preset",
+      };
+      });
+      setHousePresets(updatedPresets);
+    }
   }
 
   useEffect(() => {
     FetchData()
   },[])
 
-  let fakeData = [
-    {
-      paramName: "Casa Moderna",
-      actuatorSpec: ["Luzes", "Câmeras", "Sensores de Movimento"],
-      capacity: 5,
-      type: "preset",
-      rooms: ["Cozinha", "Sala de Estar", "Quarto Principal", "Banheiro", "Garagem"],
-    },
-    {
-      paramName: "Apartamento Compacto",
-      actuatorSpec: ["Ar Condicionado", "Fechadura Inteligente"],
-      capacity: 3,
-      type: "preset",
-      rooms: ["Sala", "Quarto", "Banheiro"],
-    },
-    {
-      paramName: "Escritório Executivo",
-      actuatorSpec: ["Projetor", "Luzes Automáticas", "Câmeras de Segurança"],
-      capacity: 10,
-      type: "preset",
-      rooms: ["Sala de Reunião", "Escritório", "Copa", "Banheiro"],
-    },
-    {
-      paramName: "Casa de Campo",
-      actuatorSpec: ["Aquecedor", "Sistema de Irrigação"],
-      capacity: 7,
-      type: "preset",
-      rooms: ["Sala", "Cozinha", "Quarto", "Banheiro", "Jardim"],
-    },
-    {
-      paramName: "Loja de Roupas",
-      actuatorSpec: ["Câmeras", "Sensores de Presença"],
-      capacity: 15,
-      type: "preset",
-      rooms: ["Área de Vendas", "Estoque", "Banheiro"],
-    },
-    {
-      paramName: "Apartamento Luxo",
-      actuatorSpec: ["Luzes Inteligentes", "Cortinas Automáticas"],
-      capacity: 4,
-      type: "preset",
-      rooms: ["Sala de Estar", "Quarto", "Banheiro", "Varanda"],
-    },
-    {
-      paramName: "Casa Familiar",
-      actuatorSpec: ["Sistema de Som", "Luzes", "Câmeras"],
-      capacity: 6,
-      type: "preset",
-      rooms: ["Sala", "Cozinha", "Quarto", "Banheiro", "Quintal"],
-    },
-    {
-      paramName: "Consultório Médico",
-      actuatorSpec: ["Ar Condicionado", "Luzes Automáticas"],
-      capacity: 8,
-      type: "preset",
-      rooms: ["Recepção", "Consultório", "Banheiro"],
-    },
-  ];
-
-  const totalPages = Math.ceil(fakeData.length / itemsPerPage);
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const currentItems = fakeData.slice(startIndex, startIndex + itemsPerPage);
+  const totalPages = Math.ceil(itemsCount / itemsPerPage);
 
   const handlePrev = () => setCurrentPage(prev => Math.max(prev - 1, 1));
   const handleNext = () => setCurrentPage(prev => Math.min(prev + 1, totalPages));
@@ -108,8 +72,8 @@ export default function ViewPreset() {
 			<section className={s.hestiaInfoWrapper}>
 				<h1>{t("viewHousePreset")}</h1>
 				<section className={s.gridWrapper}>
-					{currentItems.length > 0 ? (
-						currentItems.map((item, index) => (
+					{housePresets.length > 0 ? (
+						housePresets.map((item, index) => (
 							<ViewComponent
 								index={index}
 								title={item.paramName}
