b250225b69bab926f16badee4dab426241ab08f3
ArturMota19 <artur19rodriguesmota@gmail.com>
2025-05-16 11:32:26 -0300

    FIX: Add enviroments

--- src/services/Api.jsx
+++ src/services/Api.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
-const { VITE_BACKEND } = import.meta.env;
+const { VITE_BACKEND_DEV, VITE_BACKEND_PROD, VITE_ENV } = import.meta.env;
 
 const api = axios.create({
-  baseURL: VITE_BACKEND
+  baseURL: VITE_ENV == "DEV" ? VITE_BACKEND_DEV : VITE_BACKEND_PROD
 });
 
 
